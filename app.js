8bc1dd1239c696b7d4718cb07b91b9efeefcbda7
Belili Fahem <belili.fahem@gmail.com>
2016-05-08 01:37:50 +0200

    Test angularjs

---
+++ app.js
@@ -0,0 +1,7 @@
+/**
+ * Created by belilif on 07/05/16.
+ */
+'use strict';
+
+var app = angular.module('mytodo', []);
+
---
+++ src/controllers/todoCtlr.js
@@ -0,0 +1,22 @@
+/**
+ * Created by belilif on 07/05/16.
+ */
+'use strict';
+
+app.controller('todoIndex', function ($scope) {
+    console.log("coucou index");
+    $scope.test = 'toto';
+});
+
+/*
+app.controller('todoList', function ($scope) {
+
+});
+
+app.controller('todoCreate', function ($scope) {
+
+});
+
+app.controller('todoDelete', function ($scope) {
+
+});*/
